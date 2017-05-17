8ba7cedc938871d34357155567aba8131f960b12
Eder Diaz <ederchrono@gmail.com>
2017-05-16 21:11:20 -0500

    Add basic response to bot

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ var settings = {
 var bot = new Bot(settings);
 
 bot.on('start', function() {
-    bot.postMessageToChannel('some-channel-name', 'Hello channel!');
-    bot.postMessageToUser('some-username', 'hello bro!');
-    bot.postMessageToGroup('some-private-group', 'hello group chat!');
+    // bot.postMessageToChannel('some-channel-name', 'Hello channel!');
+    // bot.postMessageToUser('some-username', 'hello bro!');
+    // bot.postMessageToGroup('some-private-group', 'hello group chat!');
 });
 
 bot.on('message', function(data) {
     // all ingoing events https://api.slack.com/rtm 
-    console.log(data);
+    if(data.type=="message"){
+        console.log(data);
+        bot.postMessageToChannel(data.channel, 'Hola');
+    }
 });
